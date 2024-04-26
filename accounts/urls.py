a8934c3e35b5a9effb44825a93169cf2d996af16
Ankit Shukla <premusha12345@gmail.com>
2024-04-26 16:47:16 +0530

    new project

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("s/login", views.student_login, name="slogin"),  
+    path("s/register", views.student_register, name="sregister"),
+    path('logout', views.logout_view, name='logout'),
+     path('profile/create', views.create_profile, name='create_profile'),
+    
+]
