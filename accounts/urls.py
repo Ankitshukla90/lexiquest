66a26af6af71ce534ce02a8a5e0f29deb53e9a10
Ankit Shukla <premusha12345@gmail.com>
2024-04-26 22:21:28 +0530

    stuff

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("s/login", views.student_login, name="slogin"),  
     path("s/register", views.student_register, name="sregister"),
     path('logout', views.logout_view, name='logout'),
-     path('profile/create', views.create_profile, name='create_profile'),
+    path('profile/create', views.create_profile, name='create_profile'),
     
 ]
